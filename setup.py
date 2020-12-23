2fdc44188702d315ad22b49358d562347c266cbf
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-12-23 20:58:10 +0100

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pyautostart",
+    version="0.1.0",
+    description="Make executable files run after user login",
+    py_modules=["pyautostart"],
+    package_dir={"": "src"}
+)
