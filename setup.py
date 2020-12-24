7704929ef8854e15eb5b54d1a42c0261405a93a3
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-12-24 18:08:25 +0100

    Add README to setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open("README.md", "r") as file:
+    long_description = file.read()
+
 setup(
     name="pyautostart",
     version="0.1.0",
     description="Make executable files run after user login",
     py_modules=["pyautostart"],
     package_dir={"": "src"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
