21e31744c27cc0a5b23cb09c0c0ac9e19c3444e6
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-12-24 16:12:44 +0100

    Add classifiers

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ setup(
     version="0.1.0",
     description="Make executable files run after user login",
     py_modules=["pyautostart"],
-    package_dir={"": "src"}
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ]
 )
